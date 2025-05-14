53586a346acc210ab1a08fd8621ce03e3f169f44
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-14 17:26:12 +0600

    update coding

---
+++ chapter_3_datatype.js
@@ -0,0 +1,137 @@
+
+// type of // We can know data type by using typeof builtIn method 
+
+let price = 555;
+// console.log(typeof price)
+
+let learning = true;
+// console.log(typeof learning)
+
+const megaCity = "Dhaka"
+// console.log(typeof megaCity)
+
+const temperature = 102.5
+// console.log(typeof temperature)
+
+const isHungry = true;
+// console.log(typeof isHungry)
+
+
+// let : variable name no change but value is changeable 
+
+let age = 36;
+age = 35;
+// console.log(age)
+
+let pocketMoney = 500;
+pocketMoney = 150;
+// console.log(pocketMoney)
+
+let studyTime = 6;
+studyTime = 4;
+// console.log(studyTime)
+
+let friendCount = 41;
+friendCount= 38;
+// console.log(friendCount)
+
+let myPhone = 25000;
+myPhone = 12000;
+// console.log(myPhone)
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+// console.log(chairsCount)
+
+let playTime = 3;
+playTime = 0;
+// console.log(playTime)
+
+
+// comment: When a programmer is usedTo in Comment. It's help to understanding Code fully. It's a Very Good Habit.
+
+// here, score is not defined clearly. So this program is not full fill. Thats why inThisCase needs a comment
+// T20 Match targeted Score BD VS IND
+
+let score = 210;
+console.log(score)
+
+// A radius is a measure of distance from the center of any circular object to its outermost edge or boundary. 
+// A radius is not only a dimension of a circle but also for a sphere, semi-sphere, a cone with a circular base, a cylinder having circular bases.
+
+const radius = 360
+console.log(radius)
+
+let myFavfood = "Dal Rice"
+// console.log(myFavfood)
+
+let city = "Dhaka"
+let City = "Khulna"
+// console.log(city)
+// console.log(City)
+
+// SyntaxError means this is reserved key words
+// let super = "21"
+// console.log(super)
+
+// SyntaxError means this is reserved key words
+// let class = 8
+// console.log(class)
+
+// SyntaxError: Invalid or unexpected token
+// let 2ndPossition = "Pratim Mondal"
+// console.log(2ndPossition)
+
+
+// parseInt parseFloat
+
+const sugar = '30'
+// console.log(parseInt('30'))
+
+const age3 = parseInt('36')
+// console.log(age3)
+
+const fatherAge = '61'
+const fatherAgeNumber = parseInt(fatherAge);
+// console.log(fatherAgeNumber)
+
+const input = 'x20'
+const num1 = parseInt(input)
+// console.log(num1)
+
+const age4 = parseInt('djkfjdfk')
+const weight = parseInt('ten')
+// console.log(age4, weight)
+
+const age5 = parseInt('36f');
+const wight2 = parseInt('9twenty');
+const address = parseInt ('tom60')
+// console.log(age5, wight2, address)
+
+const age6 = parseInt('36.33333')
+// console.log(age6)
+
+const age7 = parseFloat('36.3333333')
+// console.log(age7)
+// console.log(age7.toFixed(2))
+
+const firstNum = 12.36251;
+const secondNum = 13.54646;
+const totalNum = firstNum + secondNum;
+// console.log(totalNum.toFixed(3))
+
+
+const num5 = '20'
+console.log(parseInt(num5) + 10)
+
+const num6 = 3.11542
+console.log(parseInt(num6))
+
+const num7 = 'premikBoy'
+console.log(parseInt(num7))
+
+const num8 = 8.2361;
+const num9 = 4.2135;
+const totalNum10 = num8 + num9;
+console.log(totalNum10.toFixed(2))
