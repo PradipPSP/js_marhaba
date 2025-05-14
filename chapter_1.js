08ec08d00c505c6562d93d0f951c8370a6169c5e
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-14 12:40:07 +0600

    finish chapter 1

--- chapter_1.js
+++ chapter_1.js
@@ -1,1 +1,55 @@
 
+// Introduce to JavaScript
+
+// Practice_1
+
+Question_1: -5, 12, -8, 0, 3.5 Find positive & Negative Numbers?
+
+Positive: 12, 3.5
+Negative: -5, -8
+
+// Here, 0 is not a Positive or Negative Number. We are Also know, Positive number is greater than 0 And Negative number is less than 0.
+
+Question_2: Find the PraseInt, Ever & Odd number from 23, 56, 19.69, 101, 42, 89, 11.37?
+
+ParseInt: 23, 56, 101, 42, 89
+Even Number: 56, 42
+Odd Number: 23, 101
+parseFloat Number: 19.69, 11.37
+
+Question_3: Find the Decimal & Fraction Number within 3.4, 7/8, 2.75, 1/3?
+
+Decimal : 3.4, 2.75
+Fraction : 7/8, 1/3
+
+Question_4: Sum 14 + 21 + 55? && 32 + 47 - 25?
+
+Sum: 90 && 54.
+
+Question_5: cost 64, Pay 100, Return how much?
+
+Return: 36
+
+Question_6: Given bonus Each Person 350, total cost?
+
+cost: 1750
+
+Question_7: Divided 450 within 8 person. how much each person in perseInt & how much in hand?
+
+EachPerson: 56
+inHand/ Remining: 2
+
+Question_8: Given Ans only true or false:
+
+15 > 12 = true
+4 < 10 = true
+7 > 9 = false
+
+Question_9: Calculate Average within 20, 25, 30, 91?
+
+Average: 41.50
+
+Question_10: Calculate the Area. Here 𝞹 value 3.14.  𝞹xr2? 
+
+Ans: 153.86
+
