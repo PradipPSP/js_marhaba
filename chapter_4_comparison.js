2a4f0337715cf9ccf9464c72370e3ed0638fb5e9
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-15 17:32:33 +0600

    1st practice done

--- chapter_4_comparison.js
+++ chapter_4_comparison.js
@@ -10,4 +10,30 @@
 // AND &&
 // OR ||
 
+let littleBro = 5;
+let elderBro = 7;
+console.log(elderBro > littleBro)
 
+let myClass = 45;
+let besideClass = 50;
+console.log(besideClass > myClass)
+
+let myMarks = 75;
+let  friendsMarks = 75;
+console.log(myMarks == friendsMarks)
+
+let mySavings = 95;
+let mySavingsTarget = 100;
+console.log(mySavings >= mySavingsTarget)
+
+let num = 10;
+let num2 = 7;
+console.log(num != num2)
+
+let num3 = 20;
+let num4 = 15;
+console.log(20 <= 15)
+
+let marks = 45;
+let fMarks = 97;
+console.log(marks <= fMarks)
