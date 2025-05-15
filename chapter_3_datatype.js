3da538b0cc72de7e687c310f980858c35ed47ea0
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-15 16:49:11 +0600

    finisingh chapter 3

--- chapter_3_datatype.js
+++ chapter_3_datatype.js
@@ -1,22 +1,4 @@
 
-// type of // We can know data type by using typeof builtIn method 
-
-let price = 555;
-// console.log(typeof price)
-
-let learning = true;
-// console.log(typeof learning)
-
-const megaCity = "Dhaka"
-// console.log(typeof megaCity)
-
-const temperature = 102.5
-// console.log(typeof temperature)
-
-const isHungry = true;
-// console.log(typeof isHungry)
-
-
 // let : variable name no change but value is changeable 
 
 let age = 36;
@@ -27,9 +9,9 @@ let pocketMoney = 500;
 pocketMoney = 150;
 // console.log(pocketMoney)
 
-let studyTime = 6;
-studyTime = 4;
-// console.log(studyTime)
+let studyTimeE = 6;
+studyTimeE = 4;
+// console.log(studyTimeE)
 
 let friendCount = 41;
 friendCount= 38;
@@ -51,20 +33,59 @@ playTime = 0;
 
 // comment: When a programmer is usedTo in Comment. It's help to understanding Code fully. It's a Very Good Habit.
 
+let score = 210;
+// console.log(score)
+
 // here, score is not defined clearly. So this program is not full fill. Thats why inThisCase needs a comment
 // T20 Match targeted Score BD VS IND
 
-let score = 210;
-console.log(score)
-
-// A radius is a measure of distance from the center of any circular object to its outermost edge or boundary. 
-// A radius is not only a dimension of a circle but also for a sphere, semi-sphere, a cone with a circular base, a cylinder having circular bases.
 
 const radius = 360
-console.log(radius)
+// console.log(radius)
 
-let myFavfood = "Dal Rice"
-// console.log(myFavfood)
+// A radius is a measure of distance from the center of any circular object to its outermost edge or boundary. 
+// A radius is not only a dimension of a circle but also for a sphere, semi-sphere, a cone with a circular base,
+// a cylinder having circular bases.
+
+let dress = 7;
+console.log(dress)
+
+/*
+I have many favorite shirt, white shirt, sky blue
+Ash color etc.
+My most favorite shirt is any one color shirt.
+*/
+
+let giftMoney = 100;
+console.log(giftMoney)
+
+// This gift was given me by my father as a good kid.
+
+let studyTime = 7;
+console.log(studyTime)
+
+// 1st 2 hours for math
+// 2nd 2 hours english
+// 3rd 3 hours coding and others
+
+let sendMoney = 12500;
+console.log(sendMoney)
+
+// mobile phone price 12000
+// service fee = 250
+// vendor = 250
+
+const pay = 724;
+console.log(pay)
+
+// I was order a bengali coding book by rokomari.com
+// This book name is Marhaba, JavaScript Maro thaba by Jhankor Mahabub. 
+// It's a very special to me when i received this book by redex courier.
+// Here, Book Price is 671, Delivery fee 53.
+
+
+let favFood = "Dal Rice"
+// console.log(favFood)
 
 let city = "Dhaka"
 let City = "Khulna"
@@ -79,15 +100,15 @@ let City = "Khulna"
 // let class = 8
 // console.log(class)
 
-// SyntaxError: Invalid or unexpected token
+// SyntaxError: Invalid or unexpected token // At first, number is not a variable name
 // let 2ndPossition = "Pratim Mondal"
 // console.log(2ndPossition)
 
 
-// parseInt parseFloat
+// parseInt parseFloat practice
 
 const sugar = '30'
-// console.log(parseInt('30'))
+console.log(parseInt('30'))
 
 const age3 = parseInt('36')
 // console.log(age3)
@@ -121,17 +142,25 @@ const secondNum = 13.54646;
 const totalNum = firstNum + secondNum;
 // console.log(totalNum.toFixed(3))
 
+// Exercise 
 
-const num5 = '20'
-console.log(parseInt(num5) + 10)
+const str = '20'
+// console.log(parseInt(str) + 10) // str convert to number and addition 10
 
-const num6 = 3.11542
-console.log(parseInt(num6))
+const modulus = 3.11542
+// console.log(parseInt(modulus)) // ParseInt
 
-const num7 = 'premikBoy'
-console.log(parseInt(num7))
+const str2 = 'premikBoy'
+console.log(parseInt(str2)) // NaN
 
-const num8 = 8.2361;
-const num9 = 4.2135;
-const totalNum10 = num8 + num9;
-console.log(totalNum10.toFixed(2))
+const num = 8.2361;
+const num2 = 4.2135;
+const sumNum = num + num2;
+console.log(sumNum.toFixed(2)) // Only decimal 2
+
+const num3 = 10.5678;
+const num4 = 5.4321;
+console.log((num3 + num4).toFixed(1))
+
+const num5 = '42.45689754'
+console.log(parseFloat(num5).toFixed(3)) // decimal 3 
