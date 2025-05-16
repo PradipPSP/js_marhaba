68a780f62a2b9ad8e9b5749d370719ee97ae8dbc
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-16 13:17:48 +0600

    practice 2 complete

--- chapter_4_comparison.js
+++ chapter_4_comparison.js
@@ -4,9 +4,9 @@
 // greater than >
 // less than <
 // Equal ==
-// Greater Than of equal >=
-// less than of equal <=
-// Not equal !=
+// Greater Than or equal >= বড় অথবা সমান কি না চেক করে
+// less than or equal <= ছোট অথবা সমান কি না চেক করে
+// Not equal != অসমান (ডিফারেন্ট কি না) কি না চেক করে
 // AND &&
 // OR ||
 
@@ -37,3 +37,36 @@ console.log(20 <= 15)
 let marks = 45;
 let fMarks = 97;
 console.log(marks <= fMarks)
+
+const myAge = 15;
+const brotherAge = 10;
+console.log(brotherAge <= myAge)
+
+const passMark = 33;
+const myMark = 45;
+const isPass = myMark >= passMark;
+console.log(isPass)
+
+const jamPrice = 30;
+const kodBelPrice = 40;
+console.log(jamPrice <= kodBelPrice)
+
+const myReadBooks = 10;
+const friendReadBooks = 10;
+console.log(myReadBooks == friendReadBooks)
+
+const myBiscuits = 15;
+const friendBiscuits = 20;
+console.log(myBiscuits < friendBiscuits)
+
+const realMadridGoal = 2;
+const barcelonaGoal = 3;
+console.log(realMadridGoal < barcelonaGoal)
+
+const myName = "Pradip Mondal"
+const sonName = "Pratim Mondal"
+console.log(myName == sonName)
+
+const myReading = true;
+const friendReading = false;
+console.log(myReading == friendReading)
