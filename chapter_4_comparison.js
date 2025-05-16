fbc30db0f786e224bf32287fea6a50c5ddf4ad1c
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-16 17:51:59 +0600

    finish coding

--- chapter_4_comparison.js
+++ chapter_4_comparison.js
@@ -79,15 +79,18 @@ const friendTk = '50'
 const js = "javaScript";
 const js2 = 'javaScript';
 //console.log(js == js2)
-//console.log(js === js2) // All Are true because value & datatype is same
+//console.log(js === js2)
+// // All Are true because value & datatype is same
 
 const test = "test"
 const test2 = "TEST"
 // console.log(test == test2)
-// console.log(test === test2) // all are false because value is not same but datatype is same
+// console.log(test === test2) 
+// // all are false because value is not same but datatype is same
 
 const x = 15;
 const y = '20'
 console.log(x != y)
-console.log(x !== y) // all are true because value and datatype is not same
+console.log(x !== y) 
+// all are true because value and datatype is not same
 
