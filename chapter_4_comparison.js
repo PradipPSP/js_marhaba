41eaa630139d28aec0cde01b65e6d9271685768c
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-16 17:50:41 +0600

    finish coding chapter 4

--- chapter_4_comparison.js
+++ chapter_4_comparison.js
@@ -12,61 +12,82 @@
 
 let littleBro = 5;
 let elderBro = 7;
-console.log(elderBro > littleBro)
+// console.log(elderBro > littleBro)
 
 let myClass = 45;
 let besideClass = 50;
-console.log(besideClass > myClass)
+// console.log(besideClass > myClass)
 
 let myMarks = 75;
 let  friendsMarks = 75;
-console.log(myMarks == friendsMarks)
+// console.log(myMarks == friendsMarks)
 
 let mySavings = 95;
 let mySavingsTarget = 100;
-console.log(mySavings >= mySavingsTarget)
+// console.log(mySavings >= mySavingsTarget)
 
 let num = 10;
 let num2 = 7;
-console.log(num != num2)
+// console.log(num != num2)
 
 let num3 = 20;
 let num4 = 15;
-console.log(20 <= 15)
+// console.log(20 <= 15)
 
 let marks = 45;
 let fMarks = 97;
-console.log(marks <= fMarks)
+// console.log(marks <= fMarks)
 
 const myAge = 15;
 const brotherAge = 10;
-console.log(brotherAge <= myAge)
+// console.log(brotherAge <= myAge)
 
 const passMark = 33;
 const myMark = 45;
 const isPass = myMark >= passMark;
-console.log(isPass)
+// console.log(isPass)
 
 const jamPrice = 30;
 const kodBelPrice = 40;
-console.log(jamPrice <= kodBelPrice)
+// console.log(jamPrice <= kodBelPrice)
 
 const myReadBooks = 10;
 const friendReadBooks = 10;
-console.log(myReadBooks == friendReadBooks)
+// console.log(myReadBooks == friendReadBooks)
 
 const myBiscuits = 15;
 const friendBiscuits = 20;
-console.log(myBiscuits < friendBiscuits)
+// console.log(myBiscuits < friendBiscuits)
 
 const realMadridGoal = 2;
 const barcelonaGoal = 3;
-console.log(realMadridGoal < barcelonaGoal)
+// console.log(realMadridGoal < barcelonaGoal)
 
 const myName = "Pradip Mondal"
 const sonName = "Pratim Mondal"
-console.log(myName == sonName)
+// console.log(myName == sonName)
 
 const myReading = true;
 const friendReading = false;
-console.log(myReading == friendReading)
+// console.log(myReading == friendReading)
+
+const myTk = 50;
+const friendTk = '50'
+// console.log(myTk == friendTk)
+// console.log(myTk === friendTk)
+
+const js = "javaScript";
+const js2 = 'javaScript';
+//console.log(js == js2)
+//console.log(js === js2) // All Are true because value & datatype is same
+
+const test = "test"
+const test2 = "TEST"
+// console.log(test == test2)
+// console.log(test === test2) // all are false because value is not same but datatype is same
+
+const x = 15;
+const y = '20'
+console.log(x != y)
+console.log(x !== y) // all are true because value and datatype is not same
+
