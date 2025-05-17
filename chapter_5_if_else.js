8414ef388d269324c27fa90f1d12f3d7fed73eb5
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-18 01:01:59 +0600

    update code

---
+++ chapter_5_if_else.js
@@ -0,0 +1,20 @@
+
+// Chapter_5 if_else
+
+// if syntax: if(condition){}
+
+// যদি কন্ডিশন সত্য হয় তবে অবজেক্টে থাকা কোড রান করবে। অন্যথায় কোন কিছু রান করবে না। 
+// মেইন কথাঃ অবশ্যই অবশ্যই কন্ডিশন সত্য হতে হবে। 
+
+// if_else syntax: if(condition){}else{}
+
+// condition true if doing work 
+// condition false else doing work
+
+const comeBack = true;
+if(comeBack < 6){
+    console.log('Have a Break First')
+} else{
+    console.log('You loose')
+}
+
