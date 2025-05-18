1288b686dfa66859f971376b45d9e03ded77e9ae
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-18 10:19:36 +0600

    update array file

---
+++ array.js
@@ -0,0 +1,19 @@
+// Array
+
+
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79]
+
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Guava"]
+
+const vegetables = ["Tomato", "Potato", "Onion", "Eggplant", "Radish", "Carrot", "Lady's Finger", "Gourd", "Cucumber", "Garlic"]
+// const size = vegetables.length
+console.log(vegetables.length)
+
+const favMovie = ["Dangal", "Animal", "Jawan", "Titanic", "Borbad"]
+
+const oddNumbers = [11, 13, 17, 19, 23, 29]
+
+const colors = ["Blue", "Red", "Pink", "White", "Black", "Yellow", "Purple"]
+
+const capitals = ["Bangladesh", "Naya Dilli", "Islamabad", "Kathmunda", "Thimpu", "Mala"]
+
