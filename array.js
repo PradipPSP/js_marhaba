cc9c5c6cac8f9a60088e09139baf0639a3e2e311
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-18 10:33:00 +0600

    update coding

--- array.js
+++ array.js
@@ -15,5 +15,31 @@ const oddNumbers = [11, 13, 17, 19, 23, 29]
 
 const colors = ["Blue", "Red", "Pink", "White", "Black", "Yellow", "Purple"]
 
-const capitals = ["Bangladesh", "Naya Dilli", "Islamabad", "Kathmunda", "Thimpu", "Mala"]
+const friendsName = ["Dinesh", "Dilip", "Debrato", "Koushik", "Sanaj", "Daud", "Santana", "Rupali", "Rokon"]
+console.log(friendsName[3])
+
+const favBooks = ["Rich Dab Poor Dak", "Ekigai", "Time Management", "Atomic Habits", "Zero to One", "The Power of Positive Thinking", "The Art of Happiness", "Miracle Morning"]
+console.log(favBooks[5])
+
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+digits[7] = 30;
+console.log(digits)
+
+const favGames = ["Free fire", "Pubji", "Candy Crush", "Tample Run"]
+favGames[2] = "Subway Sarfer"
+console.log(favGames)
+
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+digits[7] = 30;
+console.log(digits)
+
+const favGames = ["Free fire", "Pubji", "Candy Crush", "Tample Run"]
+favGames[2] = "Subway Sarfer"
+console.log(favGames)
+
+const myFamily = ["Suchitra", "Pratim", "Raju", "Radhika", "Sukumar", "Amiri", "Nitai", "Kona"]
+console.log(myFamily[3])
+
+const myVisitConuntrty = ["Nepal", "Maldives", "Thailand", "India", "New Zealand", "Bhutan"]
+console.log(myVisitConuntrty[4])
 
