f6f999fdc90ca5b37b61256979e06cf67db521d6
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-02 14:36:41 +0600

    loop practice

---
+++ loop.js
@@ -0,0 +1,35 @@
+
+// loop
+
+const numbers = [12, 95, 84, 56, 74, 15, 10];
+for (const num of numbers){
+    // console.log(num);
+};
+
+const fruits = ['apple', 'banana', 'jackfruits', 'mango'];
+for (const item of fruits){
+    // console.log(item);
+};
+
+// loof practice
+
+const colleague = ['bishonath', 'jagatbrato', 'subrata', 'provash', 'ranjit'];
+for(const offColleague of colleague){
+    // console.log(offColleague);
+};
+
+const favFoods = ['rice', 'dal', 'fish', 'meet', 'egg'];
+for(const food of favFoods){
+    // console.log(food);
+};
+
+const dateOfBirth = [1957, 1977, 1991, 1994, 2021];
+for(const year of dateOfBirth){
+    // console.log(year);
+};
+
+const favCricketTeam = ['Sachin', 'Ganguli', 'Dravid', 'Laxman', 'Gavasker'];
+for(const favTeam of favCricketTeam){
+    console.log(favTeam);
+}
+
