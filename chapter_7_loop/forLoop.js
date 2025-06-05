6e71433323f0e930e4d49d64b18224466e9fcf4e
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-06 00:54:10 +0600

    code updae

---
+++ chapter_7_loop/forLoop.js
@@ -0,0 +1,18 @@
+
+// For Loop;
+
+for (let num = 150; num<= 170; num++){
+    console.log(num)
+}
+
+let sum = 0;
+for (let i = 31; i<=58; i++){
+    sum = sum + i;
+};
+// console.log(sum); 1246
+
+let sum2 = 0;
+for (let digit = 25; digit <= 75; digit++){
+    sum2 = sum2 + digit;
+};
+console.log(sum2);
---
+++ chapter_7_loop/pairUnpair.js
@@ -0,0 +1,16 @@
+
+// Pair Un pair 
+
+// pair
+
+for(let num = 1; num<=20; num++){
+        if(num % 2 === 0){
+            console.log(num)
+        }
+}
+
+for (let i = 2; i<=20; i += 2){
+    console.log(i)
+}
+
+//
