3e023163852c3456e6ea86b9e80d9b6bb40441a0
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-06 01:50:48 +0600

    code update

---
+++ chapter_7_loop/loopBreak.js
@@ -0,0 +1,26 @@
+// Loop Break
+
+for(let i = 1; i <= 15; i++){
+    console.log(i);
+    if(i >= 7){
+        break;
+    }
+}
+
+// using continue to print pair numbers
+
+for(let i = 1; i <= 15; i++){
+    if(i % 2 == 1){
+        continue;
+    }
+    console.log(i)
+}
+
+// using continue to print pair numbers
+
+for(let i = 1; i <= 15; i++){
+    if(i % 2 == 1){
+        continue;
+    }
+    console.log(i)
+}
