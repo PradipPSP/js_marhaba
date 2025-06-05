abf1e1fea7055bd4cc25699957faefb5d4a2da2e
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-06 01:07:26 +0600

    code update

--- chapter_7_loop/pairUnpair.js
+++ chapter_7_loop/pairUnpair.js
@@ -5,12 +5,32 @@
 
 for(let num = 1; num<=20; num++){
         if(num % 2 === 0){
-            console.log(num)
+            // console.log(num)
         }
 }
 
 for (let i = 2; i<=20; i += 2){
+    // console.log(i)
+}
+
+// Not Pair
+
+for(let num = 1; num<=20; num++){
+        if(num % 2 != 0){
+            console.log(num)
+        }
+}
+
+for(let i = 1; i<=20; i +=2){
     console.log(i)
 }
 
-//
+// divided by 5
+
+for (let i = 1; i <= 30; i++){
+    if(i % 5 === 0){
+        console.log(i)
+    }
+}
+
+// divided by 3 or 5
