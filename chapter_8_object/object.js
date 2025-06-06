3ee6814a0a6303e817a363964025c463c6c5628b
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-07 02:02:36 +0600

    object practice

--- chapter_8_object/object.js
+++ chapter_8_object/object.js
@@ -75,3 +75,19 @@ const laptop = {
     price : 75000
 };
 console.log(laptop.brand);
+
+const college = {
+    name : "vnc",
+    class : ['11', '12'],
+    events : ['science fair', 'bijoy dibos', '21 fab'],
+    unique : {
+        color : 'blue',
+        result : {
+            gpa : 5,
+            merit : 'top'
+        }
+    }
+};
+console.log(college.unique.color);
+console.log(college.unique.result.merit);
+console.log(college.events[1]);
