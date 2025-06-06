250e71f94b920a1665f3cf8f336221037bafd07b
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-07 01:24:50 +0600

    object practice

---
+++ chapter_8_object/object.js
@@ -0,0 +1,23 @@
+// Object
+// Syntax: const students = {}
+
+// practice
+
+const teacher = {
+    name : "Rabin Mondal",
+    subject : "Math",
+    age : 59,
+    homeDistrict : "Satkhira",
+    schoolName : "R.K High School"
+};
+// console.log(teacher.name);
+// console.log(teacher);
+
+const tree = {
+    name : "Mango Tree",
+    age : 25,
+    height : "Eight Feet",
+    fruit : true 
+};
+console.log(tree.fruit);
+
