1266f487d7f9fc3272dc1caeb69674445f3e2a64
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-12 15:15:44 +0600

    function parameter

--- chapter_9_function/function_parameter.js
+++ chapter_9_function/function_parameter.js
@@ -1,4 +1,7 @@
 
+// Using One Parameter/ arguments
+
+
 function doubleIt(numbers){
     const doubled = numbers * 2;
     // console.log(doubled);
@@ -10,8 +13,25 @@ doubleIt(25);
 
 function square (number){
     const borGo = number * number;
-    // console.log(borGo);
+    // console.log('Square of', number, 'is:', borGo);
 };
 square(9);
 square(81);
 square(25);
+
+
+// Using Two parameter/ Arguments
+
+function add (num1, num2){
+    const sum = num1 + num2;
+    // console.log(sum);
+};
+add(15, 25);
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total);
+};
+addAll(15, 10, 15, 10, 25);
+
+
