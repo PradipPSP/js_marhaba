9dd683e33f76e2dcd1f0460f17de47d5dc6e8df0
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-12 15:06:51 +0600

    function parameter

---
+++ chapter_9_function/function_parameter.js
@@ -0,0 +1,17 @@
+
+function doubleIt(numbers){
+    const doubled = numbers * 2;
+    // console.log(doubled);
+};
+doubleIt(9);
+doubleIt(81);
+doubleIt(25);
+
+
+function square (number){
+    const borGo = number * number;
+    // console.log(borGo);
+};
+square(9);
+square(81);
+square(25);
