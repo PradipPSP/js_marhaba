a34ce46e55aa04001a4ed1c56721d1461dde7127
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-15 23:13:00 +0600

    js pricatice

--- chapter_9_function/return.js
+++ chapter_9_function/return.js
@@ -9,9 +9,32 @@ const output = tenTimes(10);
 // console.log(output); // 100
 
 
-function add(price1, price2){
+/* function add(price1, price2){
     const total = price1 + price2;
     return total;
 };
 const showResult = add(20, 30);
 console.log(showResult);
+*/
+
+// Short form
+
+function add (price1, price2){
+    return price1 + price2;
+};
+const showResult = add(20, 30);
+// console.log(showResult);
+
+
+const potato = 25;
+const onion = 62;
+const shirt = 500;
+const pant = 1250;
+
+function add (price1, price2){
+    return price1 + price2;
+};
+const bill1 = add (potato, onion);
+const bill2 = add (shirt, pant);
+const total = add (bill1, bill2);
+console.log(total);
