1fd76573ae60d8b6538070c9643cc60937adaf45
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-16 15:26:11 +0600

    funcation practice

--- chapter_9_function/return.js
+++ chapter_9_function/return.js
@@ -37,4 +37,37 @@ function add (price1, price2){
 const bill1 = add (potato, onion);
 const bill2 = add (shirt, pant);
 const total = add (bill1, bill2);
-console.log(total);
+// console.log(total);
+
+
+function doMatch (num1, num2){
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multiply = sum * diff;
+    const result = multiply / 2;
+    return result;
+};
+const result = doMatch (10, 5);
+// console.log(result);
+
+
+function isEven(number){
+    if(number % 2 ===0){
+        return true;
+    } else{
+        return false;
+    }
+};
+// console.log(isEven(8));
+// console.log(isEven(7));
+
+
+function isOdd(num){
+    if(num % 2 === 1){
+        return true;
+    } else{
+        return false;
+    }
+};
+console.log(isOdd(9));
+console.log(isOdd(8));
