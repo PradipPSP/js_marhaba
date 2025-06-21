1e4f2cf6d84787b70344af8c6af4900a9bad779e
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-21 13:34:09 +0600

    function jangson

---
+++ chapter_9_function/function_jangson.js
@@ -0,0 +1,40 @@
+
+function sumOfNumbers(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        sum += number;
+    }
+    return sum;
+};
+const num = [12, 45, 6, 8, 14];
+const total = sumOfNumbers(num);
+// console.log('Sum of Numbers =', total);
+
+
+function sumOfEvenNumber (numbers){
+    let sum = 0;
+    for(const number of numbers){
+        if(number % 2 === 0){
+            sum += number 
+        };
+    }
+    return sum
+};
+const number = [10, 30, 15, 45, 60];
+const evenTotal = sumOfEvenNumber(number);
+// console.log(evenTotal);
+
+
+function sumOfOddNumbers(numbers){
+    let sum = 0;
+    for(const num of numbers){
+        if(num % 2 === 1){
+            sum += num
+        }
+    }
+    return sum;
+};
+const numbers = [10, 15, 21, 23, 11, 20, 40];
+const oddTotal = sumOfOddNumbers(numbers);
+console.log('The Odd Numbers total =', oddTotal);
+
